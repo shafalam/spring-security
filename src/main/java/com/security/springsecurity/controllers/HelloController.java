9c8ef4e137486d8e81d50dff453236ec1a48a64b
Shafiul Alam <shafiulalam16@gmail.com>
2022-01-31 15:25:35 +0100

    wip in adding user authentication

---
+++ src/main/java/com/security/springsecurity/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.security.springsecurity.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping({"/hello"})
+    public String hello() {
+        return "Hello World";
+    }
+}
